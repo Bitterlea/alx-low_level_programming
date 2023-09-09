cbd4650398b831225bc2a5be3adea3578b1a4cce
Bitterlea <thefineseed@gmail.com>
2023-09-09 08:50:14 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print lowerCase alphabet
+ * Return: 0 if sucessfull
+ */
+int main(void)
+{
+char *str = "abcdefghijklmnopqrstuvwxyz";
+int i = 0;
+
+while (str[i] != '\0')
+{
+putchar(str[i]);
+i++;
+}
+putchar('\n');
+return (0);
+}
+ 
